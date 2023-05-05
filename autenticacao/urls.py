e942645de413812ee3e8057103b2e0d8fcbc30b4
LeonardoBaptista-dev <leonardobaptista.dev@gmail.com>
2023-05-05 09:21:27 -0300

    criando banco de dados

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.logar, name="logar"),
     path('cadastro/', views.cadastro, name="cadastro"),
     path('logar/',views.logar, name="logar"),
     path('sair/', views.sair, name="sair"),
--- sistemanutri/urls.py
+++ sistemanutri/urls.py
@@ -1,10 +1,10 @@
 
-from autenticacao import views
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('autenticacao.urls')),
-    path('', include('autenticacao.urls')),
+    path('', include('plataforma.urls'))
 ]
